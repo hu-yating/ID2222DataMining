af332e974776e606870bea7818e4cec15b492a5e
AntoniosMantzaris <mantzarisan@gmail.com>
2021-12-12 20:16:47 +0100

    tests

--- H5/src/main/java/se/kth/jabeja/Jabeja.java
+++ H5/src/main/java/se/kth/jabeja/Jabeja.java
@@ -66,7 +66,7 @@ public class Jabeja {
   */
   private double computeAcceptance(double new_val, double old_val){
       if (bonus){
-        return Math.exp(((1/old_val) - (1/new_val)) / T)
+        return Math.exp(((1/old_val) - (1/new_val)) / T);
           //return 1 / (1 + Math.exp((new_val - old_val) / T)) ; // gave bad performance
       }
       else{
